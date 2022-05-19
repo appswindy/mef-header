3742e585e8a041e531cc8093015abab914d229ab
appswindy <apps@windy.agency>
2022-05-19 13:29:09 -0300

    header

--- src/root.component.js
+++ src/root.component.js
@@ -2,9 +2,10 @@ import ButtonTest from "./buttonTest/ButtonTest";
 import NavBar from "./navBar/NavBar";
 
 export default function Root(props) {
-
-  return (<section>
-    <NavBar />
-    <ButtonTest />
-  </section>);
+  return (
+    <section>
+      <NavBar />
+      <ButtonTest />
+    </section>
+  );
 }
--- src/root.component.test.js
+++ src/root.component.test.js
@@ -4,7 +4,5 @@ import Root from "./root.component";
 describe("Root component", () => {
   it("should be in the document", () => {
     const { getByText } = render(<Root name="Testapp" />);
-    //expect(getByText(/Testapp is mounted!/i)).toBeInTheDocument();
   });
 });
-//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
